b1d0bb1a5e125c38dbf226084de4e1a0ccfcbbcf
Tyler Wallenstein <tyler@5222t.com>
2022-07-20 17:46:47 -0500

    Create sa.sh
    added

---
+++ sa.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+source /home/sa/discord.sh
+
+case $(( $RANDOM % 33 + 1 )) in
+
+ 1)
+  discord "Whoever stole the powercord to the slot machine please return it."
+  ;;
+  
+ 2)
+  discord "Please dont leave rotten food in the air vent please!"
+  ;;
+  
+ 3)
+  discord "Is this thing on?"
+  ;;
+  
+ 4)
+  dicordd "The restroom in the main hanger is flooding pelase avoid trying to flush used shell casings"
+  ;;
+  
+ 5)
+  discord "Whoever stole the tv over the bar we have you on video, if you return it now we wont launch you out the airlock"
+  ;;
+ 
+ 6)
+  discrod "If you hear a humming coming from crates do not be alarmed. We repeat. Do not be alarmed. Expect sweet things to be available in shops in the future."
+ ;;
+ 
+ 7)
+  discord "Whoever parked there ship in the no parking area you left your lights on!"
+ ;;
+ 
+ 8)
+  discord "Your an asshole!"
+ ;;
+ 
+ 9)
+  discord "The medical bay is not a parking spot. on a seperate note anyone know how to fix an IV pump?"
+ ;;
+ 
+ 10)
+   discord "You have 30 mins to move your ship!"
+   sleep 900
+   discord "You have 15 mins to move your ship!"
+   sleep 300
+   discord "You have 5 mins to move your ship!"
+   sleep 360
+   discord "Your ship has been crushed into a cube, you have 30 mins to move your cube!"
+  ;;
+ 
+ 11)
+   discord "The area marked in white is for dropoff only, there is no parking in the red area"
+  ;;
+ 
+ 12)
+  discord "The area marked in white is for dropoff only, there is no parking in the red area"
+ ;;
+ 
+ 13)
+  discord "The area marked in red is for dropoff only, there is no parking in the white area"
+ ;;
+
+ 14)
+  discord "Whoever droped there evelope labled \"awkward and embarrassing nudes\" we posted them randomly around the ship!"
+ ;;
+ 
+ 15)
+  discord "Dezz nuts!"
+  sleep 5
+  discord "Got'em"
+ ;;
+ 
+ 16)
+  discord "If your caught cheating in the casino Roland will break your legs and then you will be launched out the airlock!"
+ ;;
+ 
+ 17)
+  discord "Whoever did't wipe down the treadmill after they used it, your mom's a hoe!"
+ ;;
+
+ 18)
+  discord "Whoever ate whatever the fuck smelled like rotting fish ... good luck!"
+ ;;
+ 
+ 19)
+  discord "Main promenade's gravity has been lowered. we apologize for anybody who was on the ceiling."
+ ;;
+ 
+ 20)
+  discord "Who farted?"
+ ;;
+ 
+ 21)
+  discord "Whoever is starting the toilets on fire, get help!"
+ ;;
+ 
+ 22)
+  discord "You. yes you. report to the security office."
+ ;;
+  
+ 23)
+  discord "... so last night i was watching the security footage and i seen ... opps wrong chat!" 
+ ;;
+ 
+ 24)
+  dicord "Yo mama\!!"
+ ;;
+ 
+ 25)
+  discord "I'm afraid for the calendar. Its days are numbered."
+ ;;
+ 
+ 26)
+  discord "Singing in the shower is fun until you get soap in your mouth. Then it's a soap opera."
+ ;;
+ 
+ 27)
+  discord "What did one wall say to the other?" "I'll meet you at the corner."
+ ;;
+ 
+ 28)
+  discord "OHH it hurts so good\!\!\!"
+ ;;
+ 
+ 29)
+  discord "Harder, Harder, Keep going..."
+ ;;
+ 
+ 30) 
+  discord "The cleaning bots would like me to remind you that water fountains are not a trash can for your used condems!"
+ ;;
+ 
+ 32)
+  discord "Bingo is cancled this week due to someone parking a ship on the box with all the cards in it"
+ ;;
+ 
+ 33)
+  discord "System restarting!"
+  sleep 90
+  discord "System is back online!"
+ ;;
+ 
+ esac
+ 
+ wget -O index.html sa.sh
