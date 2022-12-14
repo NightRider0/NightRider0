0b046276c82f319f705e94d70096eef1f153f9dd
Tyler Wallenstein <tyler@5222t.com>
2022-12-14 11:44:23 -0600

    Add files via upload

---
+++ wingupdate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Written 12/14/2022 by Tyler Wallenstein
+
+ddir=`pwd`
+clear
+echo 'Hello, let me update wings for you!'
+sudo systemctl stop wings
+sudo curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
+sudo chmod u+x /usr/local/bin/wings
+systemctl restart wings
+sudo rm -Rf $ddir/wingupdate.sh
+echo ''
+echo "Done, Good bye!"
