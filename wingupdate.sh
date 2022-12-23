97b4340adea25965c9f37c16867598a6477ddb02
Tyler Wallenstein <tyler@5222t.com>
2022-12-23 00:39:53 -0600

    Update wingupdate.sh

--- wingupdate.sh
+++ wingupdate.sh
@@ -7,7 +7,7 @@ echo 'Hello, let me update wings for you!'
 sudo systemctl stop wings
 sudo curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
 sudo chmod u+x /usr/local/bin/wings
-systemctl restart wings
+sudo systemctl restart wings
 sudo rm -Rf $ddir/wingupdate.sh
 echo ''
 echo "Done, Good bye!"
